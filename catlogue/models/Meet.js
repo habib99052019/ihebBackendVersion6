2e0077203221f2e1a96d6a0e6da462d3765c1c87
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-04-01 14:20:46 +0200

    Update Meet.js

--- catlogue/models/Meet.js
+++ catlogue/models/Meet.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const  MeetSchema  = new mongoose.Schema({
+   
+    title:String,
+    Lead:{ type: Schema.Types.ObjectId, ref:'userSchema'}, 
+         employer:String,
+           id:String,
+           date:String,
+         status:String,
+         dateMetingSeconde:Number,
+          DateMeet:String,
+           Time:String 
+  });
+module.exports=mongoose.model('MeetSchema',MeetSchema);
