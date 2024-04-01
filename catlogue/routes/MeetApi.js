37c10d5b5bb90179dcccad1ef3b27d6166da172b
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-04-01 14:39:47 +0200

    Update MeetApi.js

--- catlogue/routes/MeetApi.js
+++ catlogue/routes/MeetApi.js
@@ -1,1 +1,70 @@
+const express = require('express')
+const router = express.Router();
+const lodash=require('lodash');
 
+
+const MeetSchema = require('../models/Meet')
+
+const userSchema = require('../models/userSchema')
+   router.post('/add',  async (req, res) => {
+   
+             
+// console.log(req.body , typeOf(req.body))
+    
+
+   var meet= await MeetSchema.create(req.body)
+   
+ 
+   await userSchema.findByIdAndUpdate({ _id:req.body.userPosterId}, { $push: { tableMeet: meet._id } })
+   res.send(prod)
+    
+});
+router.get('/all', async (req, res) => {
+     
+    var Meet = await MeetSchema.find().populate('lead');
+      const jsonArray = Meet.map(doc => doc.toJSON());
+     res.send(jsonArray.reverse());
+//aaaa
+ });
+router.get('/employer/:id', async (req, res) => {
+     
+    var Meet = await MeetSchema.find({employer:req.params.id}).populate('lead');
+      const jsonArray = Meet.map(doc => doc.toJSON());
+     res.send(jsonArray.reverse());
+//aaaa
+ });
+router.get('/:id', async (req, res) => {
+  console.log("habib")
+var user=  await MeetSchema.findById(req.params.id).populate('lead')
+      
+ res.send(user)
+
+
+})
+ router.put('/:id', async (req, res) => {
+  
+
+  console.log(req.params.id);
+  var  user = await MeetSchema.findByIdAndUpdate(req.params.id, req.body, { new: true })
+ 
+  res.send({message:true,user:user})
+})
+router.delete('/:id', async (req, res) => {
+    try{
+        var prod= await MeetSchema.findById(req.params.id)
+        await userSchema.findByIdAndUpdate({ _id:prod.lead}, { $pull: { tableMeet: prod._id } })
+
+        const prodDelete = await MeetSchema.deleteOne({ _id: req.params.id }).then(async (group) => {
+            var prods = await   MeetSchema.find();
+            res.send(prods)
+          })
+        
+   
+        
+    
+    }catch(error){
+        res.send(error.message)   
+    }
+    
+});
+module.exports = router;
